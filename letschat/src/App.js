903ec228ffe02377665e09c5e13eccd420e83bb3
phihainguyen <phihai93@gmail.com>
2021-02-03 22:30:01 -0800

    updating

--- letschat/src/App.js
+++ letschat/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
 	return (
 		<ChatEngine
 			height="100vh"
-			projectID="a7f7329e-4f2d-4581-a5e1-089a70a7eb44"
+			projectID={process.env.projectID}
 			userName={localStorage.getItem('username')}
 			userSecret={localStorage.getItem('password')}
 			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
