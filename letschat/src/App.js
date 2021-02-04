b257aee40f6085daf1f28150c1ee2e4f463bab70
phihainguyen <phihai93@gmail.com>
2021-02-03 21:22:29 -0800

    login form

--- letschat/src/App.js
+++ letschat/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
+	if (!localStorage.getItem('username')) return <LoginForm />;
 	return (
 		<ChatEngine
 			height="100vh"
 			projectID="a7f7329e-4f2d-4581-a5e1-089a70a7eb44"
-			userName="Rick89"
+			userName="phihai93"
 			userSecret="123123"
 			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 		/>
---
+++ letschat/src/components/LoginForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const authObject = {
+			'Project-ID'  : 'a7f7329e-4f2d-4581-a5e1-089a70a7eb44',
+			'User-Name'   : username,
+			'User-Secret' : password
+		};
+		try {
+			await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+			//storing the username and password to local storage that way once logged in you wont need to log in again
+			localStorage.setItem('username', username);
+			localStorage.setItem('password', password);
+
+			//by reloading the page you are able to render something differently after logging in
+			window.location.reload();
+		} catch (error) {}
+	};
+
+	return (
+		<div className="wrapper">
+			<div className="form">
+				<h1>Chat Application</h1>
+				<from onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+						className="input"
+						placeholder="Username"
+						required
+					/>
+					<input
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						className="input"
+						placeholder="Password"
+						required
+					/>
+					<div align="center">
+						<button type="submit" className="button">
+							<span>Let's Chat</span>
+						</button>
+					</div>
+				</from>
+			</div>
+		</div>
+	);
+};
+
+export default LoginForm;
